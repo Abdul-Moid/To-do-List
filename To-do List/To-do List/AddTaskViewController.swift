1ce180fb7c0e705cdc8dc20af9ab3ea80a8f0896
Abdul Moid Mohammed <amoid@outlook.com>
2018-02-12 23:23:55 -0600

    Created AddTaskViewController that will be used to add a task to the list.

---
+++ To-do List/To-do List/AddTaskViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddTaskViewController.swift
+//  To-do List
+//
+//  Created by Abdulmoid Mohammed on 2/12/18.
+//  Copyright © 2018 Abdul-Moid. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
